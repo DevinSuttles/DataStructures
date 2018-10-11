09d9375e00602e821b37da7c92db26992278b367
Devin Suttles <devinfnsuttles@gmail.com>
2018-10-10 22:04:58 -0500

    Initial commit

---
+++ linkedList/LinkedList.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self):
+        self.data = None # contains the data
+        self.next = None # contains the reference to the next node
+#MUST START WITH VALUE
+class LinkedList:
+    def __init__(self,value):
+        self.m_front=Node()
+        self. m_size=1
+        self.m_front.data=value
+        self.m_front.next=None
+    def getSize(self):
+        return self.m_size
+    def search(self,value):
+        temp=self.m_front
+        while temp!=None:
+            if temp.data==value:
+                return True
+            temp=temp.next
+        return False
+    def insert(self,position,value):
+        if self.m_size==1:
+            temp=Node()
+            temp.data=value
+            temp.next=self.m_front
+            self.m_front=temp
+        elif self.m_size+1==position:
+            temp=self.m_front
+            while temp.next!=None:
+                temp=temp.next
+            temp2=Node()
+            temp2.data=value
+            temp.next=temp2
+        else:
+            temp=self.m_front
+            temp2=Node()
+            for x in range(1,position):
+                temp=temp.next
+            temp2.data=value
+            temp2.next=temp.next
+            temp.next=temp2
+        self.m_size+=1
+    def getEntry(self,position):
+        temp=self.m_front
+        for i in range(1,position):
+            return temp.data
+            temp=temp.next
+    def clear(self):
+        self.m_front = None
