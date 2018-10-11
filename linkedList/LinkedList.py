73b3622227fb2f8964113866facf7ca8bfd72d66
Devin Suttles <devinfnsuttles@gmail.com>
2018-10-10 22:56:46 -0500

    Add logic to LinkedList.py

--- linkedList/LinkedList.py
+++ linkedList/LinkedList.py
@@ -2,13 +2,12 @@ class Node:
     def __init__(self):
         self.data = None # contains the data
         self.next = None # contains the reference to the next node
-#MUST START WITH VALUE
+#MUST START WITH VALUE For LinkedList
 class LinkedList:
     def __init__(self,value):
         self.m_front=Node()
-        self. m_size=1
+        self.m_size=1
         self.m_front.data=value
-        self.m_front.next=None
     def getSize(self):
         return self.m_size
     def search(self,value):
@@ -19,31 +18,32 @@ class LinkedList:
             temp=temp.next
         return False
     def insert(self,position,value):
-        if self.m_size==1:
+        if position==1:
             temp=Node()
             temp.data=value
             temp.next=self.m_front
             self.m_front=temp
-        elif self.m_size+1==position:
+        elif (self.m_size+1)==position:
             temp=self.m_front
             while temp.next!=None:
                 temp=temp.next
             temp2=Node()
             temp2.data=value
+            temp2.next=None
             temp.next=temp2
         else:
             temp=self.m_front
             temp2=Node()
-            for x in range(1,position):
+            for x in range(1,position-1):
                 temp=temp.next
             temp2.data=value
             temp2.next=temp.next
             temp.next=temp2
         self.m_size+=1
-    def getEntry(self,position):
-        temp=self.m_front
-        for i in range(1,position):
-            return temp.data
-            temp=temp.next
     def clear(self):
         self.m_front = None
+    def printList(self):
+        temp=self.m_front
+        while temp!=None:
+            print(temp.data)
+            temp=temp.next
