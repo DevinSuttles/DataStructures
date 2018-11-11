f374e469fab9e48f5681909b164702e68e5dacae
Devin Suttles <devinfnsuttles@gmail.com>
2018-11-10 19:49:48 -0600

    Change init of Queue.py

--- queue/Queue.py
+++ queue/Queue.py
@@ -3,25 +3,25 @@ class Node:
         self.data = None # contains the data
         self.next = None # contains the reference to the next node
 class Queue:
-    def __init__(self):
+    def __init__(self,value):
         self.que=Node()
+        self.que.data=value
     def isEmpty(self):
         return self.que==None
     def enqueue(self,value):
-        if self.isEmpty():
-            self.que.data=value   
         temp=self.que
         while temp.next!= None:
             temp=temp.next
         temp2=Node()
         temp2.data=value
-        temp.next=temp2 
+        temp.next=temp2
     def peek(self):
+        if self.isEmpty():
+            print("No value to peek")
         return self.que.data
-obj=Queue()
-print(obj.isEmpty())
-obj.enqueue(100)
-obj.enqueue(88)
-obj.enqueue(99999)
-obj.enqueue(8)
-print(obj.peek())
+    def dequeue(self):
+        if self.isEmpty():
+            print("Nothing to remove")
+        temp=self.que
+        temp=temp.next
+        self.que=temp
