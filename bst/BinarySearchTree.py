8d37d84d24f18fe1bb716e977f8e292a8f2ce283
Devin Suttles <devinfnsuttles@gmail.com>
2018-11-14 00:10:12 -0600

    Add height logic to BinarySearchTree.py

--- bst/BinarySearchTree.py
+++ bst/BinarySearchTree.py
@@ -1,9 +1,20 @@
 class BinaryNode:
     def __init__(self):
-        self.right=None
-        self.left=None
+        self.rightChild=None
+        self.leftChild=None
         self.data=None
 class BinarySearchTree:
     def __init__(self,value):
-        self.m_root=BinaryNode()
-        self.m_root.data=value
+        self.root=BinaryNode()
+        self.root.data=value
+    def isEmpty(self):
+        return self.root==None
+    def height(self):
+        if self.isEmpty():
+            raise Exception("No value in binary tree so height is zero.")
+        return self._height(self.root)
+    def _height(self,root):
+        if root==None:
+            return 0
+        else:
+            return 1+self._height(root.leftChild)+self._height(root.rightChild)
