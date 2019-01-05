486bcb093c913fe3f09ae3880334d4b42f6bd62b
Devin Suttles <devinfnsuttles@gmail.com>
2019-01-04 21:56:33 -0700

    Add insert and getSize to MaxHeap.py

--- max/MaxHeap.py
+++ max/MaxHeap.py
@@ -1,5 +1,9 @@
 class MaxHeap(object):
     def __init__(self):
-        self.list=None
+        self.list=[]
         self.size=0
-    
+    def insert(self,value):
+        self.list.append(value)
+        self.size+=1
+    def getSize(self):
+        return self.size
