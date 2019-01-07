aabed58c0a204fe1581a2dcd511b86ad7f3d9dcd
Devin Suttles <devinfnsuttles@gmail.com>
2019-01-07 00:24:41 -0700

    Add getSize, peek, and push to MaxHeap.py

--- max/MaxHeap.py
+++ max/MaxHeap.py
@@ -1,9 +1,21 @@
 class MaxHeap(object):
+
     def __init__(self):
         self.list=[]
-        self.size=0
-    def insert(self,value):
+        self.size=-1
+
+    def push(self,value):
         self.list.append(value)
         self.size+=1
+        if self.size!=0:
+
+#returns the size of the heap
     def getSize(self):
         return self.size
+
+#gives back the MaxValue
+    def peek(self):
+        if self.size != -1:
+            return self.list[0]
+
+max=MaxHeap()
